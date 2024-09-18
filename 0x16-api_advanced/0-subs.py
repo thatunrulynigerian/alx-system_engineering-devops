0a07d01eb2e89a297d39d1932a9d8c142f247406
Maria Ukpere <mukpere777@gmail.com>
2024-09-18 21:47:13 +0000

    changes

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,17 +8,22 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Returns the number of subscribers for a given subreddit."""
-    if subreddit is None or not isinstance(subreddit, str):
+    if not isinstance(subreddit, str) or not subreddit:
         return 0
     
-    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': '0x16-api_advanced:project:v1.0.0 (by /u/firdaus_cartoon_jr)'}
-    response = requests.get(url, headers=headers)
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {
+        'User-Agent': '0x16-api_advanced:project:v1.0.0 (by /u/firdaus_cartoon_jr)'
+    }
+    
+    # Make the GET request without allowing redirects
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     # Check if the request was successful
     if response.status_code != 200:
         return 0
 
+    # Parse the JSON response
     data = response.json()
     subs = data.get("data", {}).get("subscribers", 0)
     return subs
